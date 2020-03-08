c2f06cd234025b3c8478f16ac3b95ec47771908e
Valentino Pesce <valentino@iltuobrand.it>
2020-03-08 16:40:24 +0100

    Remove bar color and update title

--- public/js/china.js
+++ public/js/china.js
@@ -14,12 +14,10 @@ function getChina(data) {
                 data: [confirmed, deaths],
                 backgroundColor: [
                     'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
                     'rgba(255, 99, 132, 0.2)'
                 ],
                 borderColor: [
                     'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
                     'rgba(255, 99, 132, 1)'
                 ],
                 borderWidth: 2
