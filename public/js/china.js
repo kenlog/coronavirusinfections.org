0ede09927fcd042982d2cd0c7e164149bd9ac65b
Valentino Pesce <valentino@iltuobrand.it>
2020-03-08 16:35:19 +0100

    Remove var recovered

--- public/js/china.js
+++ public/js/china.js
@@ -3,16 +3,15 @@ d3.csv(file).then(getChina);
 function getChina(data) {
     var confirmed = data.map(function (d) { return d.Confirmed });
     var deaths = data.map(function (d) { return d.Deaths });
-    var recovered = data.map(function (d) { return d.Recovered });
 
     var ctx = document.getElementById('chartChina');
     var chartChina = new Chart(ctx, {
         type: 'horizontalBar',
         data: {
-            labels: ['Confirmed', 'Recovered', 'Deaths'],
+            labels: ['Confirmed', 'Deaths'],
             datasets: [{
                 label: 'Covid-19 ',
-                data: [confirmed, recovered, deaths],
+                data: [confirmed, deaths],
                 backgroundColor: [
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
