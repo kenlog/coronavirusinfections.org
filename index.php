92267c45f5f1a26b0de4b045db93db847145d557
Valentino Pesce <valentino@iltuobrand.it>
2020-03-11 13:06:09 +0100

    Add Favicon

--- index.php
+++ index.php
@@ -30,9 +30,25 @@ $contagionDays = count(glob(dirname(__FILE__) . '/data/csv/reports/*'));
     <link rel="stylesheet" href="public/css/fontawesome-all.css">
     <link rel="stylesheet" href="public/css/bootstrap-table.min.css">
     <link rel="stylesheet" href="public/css/style.css">
-    <meta name="theme-color" content="#1b1e23">
     <title>Coronavirus infections (COVID-19)</title>
     <meta name="description" content="Health information on coronavirus infections with detailed live statistics from around the world.">
+    <link rel="apple-touch-icon" sizes="57x57" href="public/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="public/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="public/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="public/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="public/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="public/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="public/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="public/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="public/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="public/icons//android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="public/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="public/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="public/icons/favicon-16x16.png">
+    <link rel="manifest" href="public/icons/manifest.json">
+    <meta name="msapplication-TileColor" content="#1b1e23">
+    <meta name="msapplication-TileImage" content="public/icons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#1b1e23">
 </head>
 
 <body>
