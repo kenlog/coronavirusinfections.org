9b52ac0a3740e4679311ffb2f5605d56f1bcb053
Valentino Pesce <valentino@iltuobrand.it>
2020-03-15 11:44:52 +0100

    Add source of news

--- includes/heading.php
+++ includes/heading.php
@@ -19,6 +19,7 @@
                 <p class="lead">The outbreak was declared a Public Health Emergency of International Concern on 30 January 2020.</p>
                 <p class="lead">On 11 February 2020, WHO announced a name for the new coronavirus disease: COVID-19</p>
                 <p class="lead">On 11 March 2020, "WHO has been assessing this outbreak around the clock and we are deeply concerned both by the alarming levels of spread and severity, and by the alarming levels of inaction. We have therefore made the assessment that COVID-19 can be characterized as a pandemic."</p>
+                <small>- Press releases from the WHO website -</small>
             </div>
         </div>
 
