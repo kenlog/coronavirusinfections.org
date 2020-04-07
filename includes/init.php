4ae6d00168548096ffe12e6b8f3991aae66f6da5
Valentino Pesce <valentino@iltuobrand.it>
2020-04-07 09:55:16 +0200

    Add report 6 April 2020

--- includes/init.php
+++ includes/init.php
@@ -12,9 +12,9 @@
 
 $host = "https://coronavirusinfections.org";
 
-$updateDate = '5 April 2020';
+$updateDate = '6 April 2020';
 
-$nextUpdate = '10 AM CET 7 April 2020'; 
+$nextUpdate = '10 AM CET 8 April 2020'; 
 
 $pathReportFolder = 'data/csv/reports/';
 
